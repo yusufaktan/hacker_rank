634808c00cca5c4644e74b2efb5cad50e879b4d9
Yusuf Aktan <ycan2497@gmail.com>
2023-01-28 19:16:05 +0300

    solved

---
+++ time_conversion.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    if (s[8] == "P"):
+        if (s[:2] == "12"):
+            return s[:-2]
+        else:
+            return str(int(s[:2]) + 12) + s[2:-2]
+    else:
+        if (s[:2] == "12"):
+            return "00" + s[2:-2]
+        else:
+            return s[:-2]
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
